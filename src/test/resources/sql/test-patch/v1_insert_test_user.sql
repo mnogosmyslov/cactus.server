f8f50e61f0888541946bc940e6b1d4a8e64ea0dc
Alexander Mnogosmyslov <mnogosmyslov@hotmail.com>
2015-01-26 13:43:09 +0200

    Insert test user patch

---
+++ src/test/resources/sql/test-patch/v1_insert_test_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO users(id, email, login, password, photo) VALUES(
+    9999,
+    'test@mail.com',
+    'test',
+    'b1b3773a05c0ed0176787a4f1574ff0075f7521e',
+    'http://localhost:8080/cactus/pages/resources/tmp/photo.png'
+)
