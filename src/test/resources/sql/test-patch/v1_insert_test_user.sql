86ed3a12a47d265549ede38cae7eeb572719bf1c
Alexander Mnogosmyslov <mnogosmyslov@hotmail.com>
2015-01-31 12:30:27 +0200

    Fix user patch

--- src/test/resources/sql/test-patch/v1_insert_test_user.sql
+++ src/test/resources/sql/test-patch/v1_insert_test_user.sql
@@ -1,7 +1,8 @@
-INSERT INTO users(id, email, login, password, photo) VALUES(
+INSERT INTO users(id, email, login, password, photo, role) VALUES(
     9999,
     'test@mail.com',
     'test',
     'b1b3773a05c0ed0176787a4f1574ff0075f7521e',
-    'http://localhost:8080/cactus/pages/resources/tmp/photo.png'
+    'http://localhost:8080/cactus/pages/resources/tmp/photo.png',
+    'ROLE_USER'
 )
