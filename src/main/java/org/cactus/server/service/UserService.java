808c41def236ecc92f2a53211a8023c0a7ecee6b
AlexanderVoV <sashafox13@gmail.com>
2015-04-06 20:17:23 +0300

    addition

---
+++ src/main/java/org/cactus/server/service/UserService.java
@@ -0,0 +1,18 @@
+package org.cactus.server.service;
+
+import org.cactus.server.entity.UserAccount;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserService {
+
+    UserAccount getAccount(String email);
+    UserAccount getById(long id);
+    UserAccount getByLogin(String login);
+    void createUserAccount(UserAccount userAccount) throws SQLException;
+    void updateUserAccount(UserAccount userAccount) throws SQLException;
+    void deleteUserAccount(long id) throws SQLException;
+    List getAllUserAccount();
+
+}
