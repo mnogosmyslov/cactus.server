b5c677898c5f3b9b56b255323ad29a167faf2898
Alexander Mnogosmyslov <mnogosmyslov@hotmail.com>
2015-04-15 17:58:40 +0300

    Implementation methods for subscribe user on their WebSockets

--- src/main/java/org/cactus/server/controller/ChatController.java
+++ src/main/java/org/cactus/server/controller/ChatController.java
@@ -1,17 +1,41 @@
 package org.cactus.server.controller;
 
 import org.cactus.server.api.ChatApi;
+import org.cactus.server.entity.Chat;
+import org.cactus.server.entity.Message;
+import org.cactus.server.entity.UserAccount;
+import org.cactus.server.service.ChatService;
+import org.cactus.server.service.UserService;
 import org.cactus.share.vo.MessageVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping(ChatApi.CHAT)
 public class ChatController {
+
+    @Autowired
+    private UserService userAccountService;
+
+    @Autowired
+    private ChatService chatService;
+
+    private final SimpMessageSendingOperations messagingTemplate;
+    private List<String> chats = new ArrayList<String>();
 
     @MessageMapping("/chat")
     @SendTo("/conversation")
@@ -21,6 +45,39 @@ public class ChatController {
             messageVO.setDate(new Date());
         }
         return messageVO;
+    }
+
+    @Autowired
+    public ChatController(SimpMessageSendingOperations messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @SubscribeMapping("/join")
+    public List<String> join(Principal principal) {
+
+        messagingTemplate.convertAndSend("/chat/join", principal.getName());
+
+        return chats;
+    }
+
+    @MessageMapping("/chat/{username}")
+    public void chatReveived(org.springframework.messaging.Message msg, @Payload Message message, @DestinationVariable("username") String username) {
+        UserAccount userAccount = userAccountService.getByLogin(username);
+        message.setAuthorId(userAccount.getId());
+
+        LinkedMultiValueMap<String, String> headers = (LinkedMultiValueMap<String, String>) msg.getHeaders().get("nativeHeaders");
+        String s = headers.getFirst("destination");
+        long chatId = Long.valueOf(s);
+        Chat chat = chatService.getChat(chatId);
+
+        Set<UserAccount> hset = chat.getMembers();
+
+        List<String> members = new ArrayList<String>();
+        // get members from chat by chatId
+
+        for (String member : members) {
+            messagingTemplate.convertAndSendToUser(member, "/chat/" + member, message);
+        }
     }
 
 }
---
+++ src/test/resources/sql/test-patch/v2_insert_test_chats.sql
@@ -0,0 +1,23 @@
+INSERT INTO chat(chatid, last_message, secure) VALUES(
+    100,
+    14,
+    'false'
+);
+
+INSERT INTO chat(chatid, last_message, secure) VALUES(
+    102,
+    10,
+    'false'
+);
+
+INSERT INTO chat(chatid, last_message, secure) VALUES(
+    101,
+    14,
+    'false'
+);
+
+INSERT INTO chat(chatid, last_message, secure) VALUES(
+    103,
+    12,
+    'false'
+);
